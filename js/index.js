e59bb5afde03b317252cfbfe1dac6916da0daef5
jonathancklee1 <jonathancklee@gmail.com>
2022-03-06 13:48:10 +1100

    Fixed aside layout Implemented nav down btn

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const closeBtn = document.getElementById("mobile-close-icon");
 const mobileNav = document.querySelector(".mobile-nav-bar");
 const hamburgerIcon = document.querySelector(".hamburger-icon");
+const navDownBtn = document.querySelector(".nav-down-btn");
+const movieList = document.querySelector(".movie-list-section");
 
 hamburgerIcon.addEventListener("click", () => {
   mobileNav.classList.add("open");
@@ -8,4 +10,11 @@ hamburgerIcon.addEventListener("click", () => {
 
 closeBtn.addEventListener("click", () => {
   mobileNav.classList.remove("open");
+});
+navDownBtn.addEventListener("click", () => {
+  movieList.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "nearest",
+  });
 });
